abc692af2987bf8eba353f968cd95a0a639e96a5
Davide G. Allegra <64493007+Davixss@users.noreply.github.com>
2023-08-21 17:57:45 +0200

    Delete Code/Arduino directory

--- Code/Arduino/Arduino Reader (.py)/Config.py
+++
@@ -1,8 +0,0 @@
-# Client MQTT configuration
-
-class Config:
-    endpoint = "apv9omwb522qd-ats.iot.eu-central-1.amazonaws.com"
-    client_id = "ArduinoUnoR3"
-    root_ca = "keys/AWS-Root-RSA.pem"
-    private_key = "keys/AWS-private.pem.key"
-    certificate = "keys/AWS-certificate.pem.crt"
